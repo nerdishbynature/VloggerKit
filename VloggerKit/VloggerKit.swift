76e1b222db423bcf19aa05352236977046601f27
Piet Brauer <piet@nerdishbynature.com>
2017-01-19 13:32:59 +0100

    Add trailing slashes in URL

--- VloggerKit/VloggerKit.swift
+++ VloggerKit/VloggerKit.swift
@@ -1,8 +1,8 @@
 import Foundation
 import RequestKit
 
-let GoogleOAuthBaseURL = "https://accounts.google.com/o/oauth2"
-let YouTubeBaseURL = "https://www.googleapis.com"
+let GoogleOAuthBaseURL = "https://accounts.google.com/o/oauth2/"
+let YouTubeBaseURL = "https://www.googleapis.com/"
 let VloggerKitErrorDomain = "com.nerdishbynature.vloggerkit.error"
 
 public struct VloggerKit {
